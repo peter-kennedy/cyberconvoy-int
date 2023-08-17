7cafb54b98ce7fed85bccae280e27711e24fd383
Peter Kennedy <pkennedy97@gmail.com>
2023-08-17 16:07:41 -0400

    adds toggle button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+import { useState } from 'react'
 import PhotoContainer from './PhotoContainer.tsx';
 
 function App() {
-
+  //typically would not keep state in app component and would just render one 'main container', 
+  //but for the sake of time and getting functionality down I am adding this
+  const [ showPhotos, setShowPhotos ] = useState(false);
   return (
     
     <div className="App">
       <div className="app-container">
         <h1>cyberconvoy.dev</h1>,
         <div className="photo-container">
-          <PhotoContainer />
+          {showPhotos ? 
+            <PhotoContainer /> :
+            null
+          }
+          <button onClick={() => setShowPhotos(!showPhotos)}>Show Photos</button>
         </div>
       </div>
     </div>
